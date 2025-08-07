ae1f8bddbd9c67c2521fe9bdf9a5e363bd41c4f5
J.M. Wilyat <jmwilyat@gmail.com>
2025-08-06 23:04:22 -0700

    chore: add public shell component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 // src/App.tsx
-import { Authenticator } from '@aws-amplify/ui-react';
+import { useState } from 'react';
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react';
+
 import AuthenticatedShell from './pages/AuthenticatedShell';
+import PublicShell from './pages/PublicShell';
 
 export default function App() {
+  return (
+    <Authenticator.Provider>
+      <AppContent />
+    </Authenticator.Provider>
+  );
+}
+
+function AppContent() {
+  const { authStatus } = useAuthenticator((ctx) => [ctx.authStatus]);
+  const [loginTriggered, setLoginTriggered] = useState(false);
+
+  if (authStatus === 'authenticated') {
+    return <AuthenticatedShell />;
+  }
+
+  if (!loginTriggered) {
+    return <PublicShell onRequireAuth={() => setLoginTriggered(true)} />;
+  }
+
   return (
     <Authenticator>
       <AuthenticatedShell />
