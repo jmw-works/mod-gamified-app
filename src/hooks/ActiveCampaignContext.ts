a5cc6d12b9858d317740b55b3b4193a0de671e11
J.M. Wilyat <jmwilyat@gmail.com>
2025-08-06 21:06:04 -0700

    purge

--- src/hooks/ActiveCampaignContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-export type ActiveCampaignContextType = {
-  activeCampaignId: string | null;
-  setActiveCampaignId: (id: string | null) => void;
-};
-
-export const ActiveCampaignContext = createContext<ActiveCampaignContextType | undefined>(undefined);
